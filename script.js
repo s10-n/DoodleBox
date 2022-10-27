359144b47cb8ce6091eca939b66b10bde2ba1707
Sean Beaudoin <s10_n@mailbox.org>
2022-10-26 22:02:32 -0700

    Create for loop to create grid square divs

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const NUMBER_OF_SQUARES = 16;
+
 let gridContainer = document.querySelector("#grid-container");
+
+// create a for loop that iterates NUMBER_OF_SQUARES times and creates a square for each one, then appends that square to the grid container.
+
+for (let i = 0; i < NUMBER_OF_SQUARES; i++) {
+    
+}
